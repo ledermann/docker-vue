543ddab93a9495b1f6ebdbe09865a543062d1155
Georg Ledermann <mail@georg-ledermann.de>
2018-02-27 13:01:39 +0100

    move code into views

--- src/views/PostShow.vue
+++ src/views/PostShow.vue
@@ -1,15 +1,38 @@
 <template>
-  <Post :slug='slug'></Post>
+  <div class="content">
+    <h1 class="title">{{ post.title }}</h1>
+
+    <p>
+      <b-tag type="is-dark" v-if="post.updated_at">
+        {{ post.updated_at | moment("from", "now") }}
+      </b-tag>
+    </p>
+
+    <div v-html="post.content"></div>
+
+    <hr />
+
+    <div v-html="post.copyright"></div>
+  </div>
 </template>
 
 <script>
-import Post from '../components/Post.vue'
+import axios from 'axios'
 
 export default {
   name: 'PostShow',
+
   props: ['slug'],
-  components: {
-    Post
+
+  data() {
+    return {
+      post: {}
+    }
+  },
+
+  mounted() {
+    axios.get('https://docker-rails.georg-ledermann.de/posts/'+this.slug+'.json').
+      then(response => this.post = response.data)
   }
 }
 </script>
