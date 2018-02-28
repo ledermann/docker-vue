1a3323bccddd9a8732bce95172822d11b00c482a
Georg Ledermann <mail@georg-ledermann.de>
2018-02-28 15:18:23 +0100

    PostsIndex: Add infinite scrolling

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -1,39 +1,64 @@
 <template>
-  <b-table :hoverable="true" :loading="isLoading" :data="posts">
-    <template slot-scope="props">
-      <b-table-column label="Content">
-        <b-icon v-if="props.row.clips_count > 0"
-            pack="fas"
-            icon="image"
-            size="is-small">
-        </b-icon>
-        <strong>
-          <router-link :to="{ name: 'post', params: {slug: props.row.slug} }">
-            {{ props.row.title }}
-          </router-link>
-        </strong>
-        {{ props.row.content }}
-      </b-table-column>
+  <div>
+    <b-table :hoverable="true" :loading="isLoading" :data="posts">
+      <template slot-scope="props">
+        <b-table-column label="Content">
+          <b-icon v-if="props.row.clips_count > 0"
+              pack="fas"
+              icon="image"
+              size="is-small">
+          </b-icon>
+          <strong>
+            <router-link :to="{ name: 'post', params: {slug: props.row.slug} }">
+              {{ props.row.title }}
+            </router-link>
+          </strong>
+          {{ props.row.content }}
+        </b-table-column>
 
-      <!-- eslint-disable-next-line -->
-      <b-table-column type="is-narrow" label="Last update">
-        <b-tag type="is-dark">
-          <timeago :since="props.row.updated_at"></timeago>
-        </b-tag>
-      </b-table-column>
-    </template>
-  </b-table>
+        <!-- eslint-disable-next-line -->
+        <b-table-column type="is-narrow" label="Last update">
+          <b-tag type="is-dark">
+            <timeago :since="props.row.updated_at"></timeago>
+          </b-tag>
+        </b-table-column>
+      </template>
+    </b-table>
+
+    <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import InfiniteLoading from 'vue-infinite-loading';
 
 export default {
   name: 'PostsIndex',
 
+  components: {
+    InfiniteLoading,
+  },
+
+  methods: {
+    infiniteHandler($state) {
+      this.currentPage++;
+      axios.get('https://docker-rails.georg-ledermann.de/posts.json?page=' + this.currentPage)
+        .then(response => {
+          if (response.data.length > 0) {
+            this.posts.push(...response.data);
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        })
+    },
+  },
+
   data () {
     return {
       isLoading: true,
+      currentPage: 1,
       posts: []
     }
   },
