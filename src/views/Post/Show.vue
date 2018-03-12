ae5f5d43e9c76b63950b049a9d7265c97b72e92f
Georg Ledermann <mail@georg-ledermann.de>
2018-03-12 15:54:24 +0100

    Error handling

--- src/views/Post/Show.vue
+++ src/views/Post/Show.vue
@@ -1,52 +1,71 @@
 <template>
   <layout-basic>
-    <div slot="hero-body" class="container has-text-centered">
-      <h1 class="title">
-        {{ post.title }}
-      </h1>
-    </div>
 
-    <div slot="hero-foot" class="container">
-      <div class="tabs is-boxed is-centered">
+    <template v-if="isSuccess">
+      <div slot="hero-body" class="container has-text-centered">
+        <h1 class="title">
+          {{ post.title }}
+        </h1>
+      </div>
+
+      <div slot="hero-foot" class="container">
+        <div class="tabs is-boxed is-centered">
+          <ul>
+            <router-link tag="li" :to="'/posts/' + post.slug" class="is-active" exact>
+              <a>Article</a>
+            </router-link>
+
+            <router-link tag="li" :to="'/posts/' + post.slug + '/audits'" exact>
+              <a>History</a>
+            </router-link>
+          </ul>
+        </div>
+      </div>
+
+      <b-loading :active="isLoading"></b-loading>
+
+      <div class="content">
+        <p>
+          <b-tag type="is-dark" v-if="post.updated_at">
+            <timeago :since="post.updated_at"></timeago>
+          </b-tag>
+        </p>
+
+        <silentbox-group>
+            <silentbox-item v-for="(clip,index) in clips" :src="clip.urlLarge" v-bind:key="index">
+              <figure class="image is-128x128">
+                <img :src="clip.urlThumb">
+              </figure>
+            </silentbox-item>
+        </silentbox-group>
+
+        <hr />
+
+        <div v-html="post.content"></div>
+
+        <hr />
+
+        <div class="has-text-grey" v-html="post.copyright"></div>
+
+        <hr />
+      </div>
+    </template>
+
+    <template v-else>
+      <div slot="hero-body" class="container has-text-centered">
+        <h1 class="title">
+          ERROR
+        </h1>
+      </div>  
+
+      <div class="content">
         <ul>
-          <router-link tag="li" :to="'/posts/' + post.slug" class="is-active" exact>
-            <a>Article</a>
-          </router-link>
-
-          <router-link tag="li" :to="'/posts/' + post.slug + '/audits'" exact>
-            <a>History</a>
-          </router-link>
+          <li v-for="error of errors">
+            {{ error.message }}
+          </li>
         </ul>
       </div>
-    </div>
-
-    <b-loading :active="isLoading"></b-loading>
-
-    <div class="content" v-if="!isLoading">
-      <p>
-        <b-tag type="is-dark" v-if="post.updated_at">
-          <timeago :since="post.updated_at"></timeago>
-        </b-tag>
-      </p>
-
-      <silentbox-group>
-          <silentbox-item v-for="(clip,index) in clips" :src="clip.urlLarge" v-bind:key="index">
-            <figure class="image is-128x128">
-              <img :src="clip.urlThumb">
-            </figure>
-          </silentbox-item>
-      </silentbox-group>
-
-      <hr />
-
-      <div v-html="post.content"></div>
-
-      <hr />
-
-      <div class="has-text-grey" v-html="post.copyright"></div>
-
-      <hr />
-    </div>
+    </template>
   </layout-basic>
 </template>
 
@@ -70,6 +89,7 @@ export default {
   data () {
     return {
       isLoading: true,
+      errors: [],
       currentSlug: '',
       post: {}
     }
@@ -82,16 +102,27 @@ export default {
   methods: {
     loadData: function (slug) {
       this.isLoading = true
-      this.$http.get('/posts/' + slug + '.json')
+      this.$http.get('/posts/' + slug)
         .then(response => {
           this.post = response.data
           this.currentSlug = slug
           this.isLoading = false
+          this.errors = []
+        })
+        .catch(error => {
+          this.post = null
+          this.currentSlug = null
+          this.isLoading = false
+          this.errors.push(error)
         })
     }
   },
 
   computed: {
+    isSuccess: function() {
+      return (this.errors.length == 0)
+    },
+
     clips: function () {
       if (this.post.clips) {
         return this.post.clips.map(clip => {
