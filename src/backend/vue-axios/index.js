ab82612782ec746b218788fdc60d91d75c3be3ff
Georg Ledermann <mail@georg-ledermann.de>
2018-03-13 14:31:50 +0100

    Change API endpoint

--- src/backend/vue-axios/index.js
+++ src/backend/vue-axios/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
 
-const API_URL = process.env.API_URL || 'https://docker-rails.georg-ledermann.de'
+const API_URL = process.env.API_URL || 'https://docker-rails.georg-ledermann.de/api/v1'
 
 const customAxios = axios.create({
   baseURL: API_URL,
