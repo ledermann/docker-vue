bd53010165b5231382df69c4a4b9d7271f1e6361
Georg Ledermann <mail@georg-ledermann.de>
2018-03-15 10:31:08 +0100

    Axios: Always send auth token, if there is any

--- src/backend/vue-axios/index.js
+++ src/backend/vue-axios/index.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
 
-const API_URL = process.env.API_URL || 'https://docker-rails.georg-ledermann.de/api/v1'
+axios.defaults.baseURL = process.env.API_URL || 'https://docker-rails.georg-ledermann.de/api/v1'
+axios.defaults.headers.post['Accept'] = 'application/json'
 
-const customAxios = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Accept': 'application/json'
+axios.interceptors.request.use(function (config) {
+  // Auth auth token before request is sent
+  if (localStorage.token) {
+    config.headers.common['Authorization'] = 'Bearer ' + localStorage.token
   }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
 })
 
-Vue.use(VueAxios, customAxios)
+Vue.use(VueAxios, axios)
