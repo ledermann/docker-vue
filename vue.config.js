006f4a756d73996c80ad54cf620a5a50cd04e9c0
Georg Ledermann <mail@georg-ledermann.de>
2018-05-11 08:58:36 +0200

    :arrow_up: yarn upgrade

--- vue.config.js
+++ vue.config.js
@@ -3,21 +3,3 @@
 const fs = require('fs')
 const json = JSON.stringify({ timestamp: new Date() })
 fs.writeFile('src/build-time.json', json, () => {})
-
-// Add path for postcss-loader
-// Otherwise Docker build will fail
-const merge = require('webpack-merge')
-module.exports = {
-  chainWebpack: config => {
-    config.module
-      .rule('css')
-      .use('postcss-loader')
-      .tap(options =>
-        merge(options, {
-          config: {
-            path: '.postcssrc'
-          }
-        })
-      )
-  }
-}
