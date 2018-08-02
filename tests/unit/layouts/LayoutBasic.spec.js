7cbbf2a4cb24e68f4d69dda1eb9a1f9148a87ce3
Georg Ledermann <mail@georg-ledermann.de>
2018-08-02 06:29:52 +0200

    Fix warning about "shallow"

--- tests/unit/layouts/LayoutBasic.spec.js
+++ tests/unit/layouts/LayoutBasic.spec.js
@@ -1,10 +1,10 @@
 import { expect } from 'chai'
-import { shallow } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import LayoutBasic from '@/layouts/LayoutBasic.vue'
 
 describe('LayoutBasic.vue', () => {
   it('renders hero section', () => {
-    const wrapper = shallow(LayoutBasic, {
+    const wrapper = shallowMount(LayoutBasic, {
     })
     expect(wrapper.html()).to.include('<section class="hero is-info">')
   })
